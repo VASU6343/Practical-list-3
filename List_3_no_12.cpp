b66a577b34a157ef471903ea85ca93e5e7f3ea66
Vasu Sapariya <141891510+Vasu1309@users.noreply.github.com>
2023-09-18 11:01:39 +0530

    List_3_no_12.cpp

---
+++ List_3_no_12.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class item
+{
+	private:
+		int id;
+		char name[100];
+		float price;
+	public:
+		void getdata();
+		void putdata()
+		{
+      cout<<"220130318016";
+      cout<<"Vasu Sapariya";
+			cout<<"Product_id is::"<<id<<endl;
+			cout<<"Product_name is::"<<name<<endl;
+			cout<<"Product_price is::"<<price<<endl;
+		}
+};
+void item::getdata()
+{
+			cout<<"Enter the Product_id::";
+			cin>>id;
+			cout<<"Enter the Product_name::";
+			cin>>name;
+			cout<<"Enter the Product_price::";
+			cin>>price;	
+}
+int main()
+{	
+	item t1;
+	t1.getdata();
+	t1.putdata();
+	return 0;
+}
